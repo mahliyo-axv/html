8ab157a6c79b16d22e37881495fc91547bf91750
mahliyo-axv <mahliyo.axv@gmail.com>
2026-02-18 09:58:40 +0300

    Add files via upload

---
+++ lesson 1/index.js
@@ -0,0 +1,12 @@
+let userName = "mahliyo";
+let userAge = 23;
+let userEmail = "mahliyo.axv@gmail.com"
+let userIsAdmin = true
+console.log(userName+ " "+userAge+ " " +userEmail+ " " +userIsAdmin )
+console.log(5 + 5)
+console.log(101 - 3)
+console.log(9087 * 3 )
+console.log(242686 / 2 )
+let num1 = prompt("son 1");
+let num2 = prompt("son 2");
+console.log(Number(num1) + Number (num2));

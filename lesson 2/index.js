0c87e8c8ba9f3c11485351ce471f9e63cc7a62d8
mahliyo-axv <mahliyo.axv@gmail.com>
2026-03-02 12:02:34 +0500

    Add files via upload

---
+++ lesson 2/index.js
@@ -0,0 +1,15 @@
+let temp = 15;
+if(temp > 0){
+    console.log("Qalin kiyinib oling ko'chada qor");
+}
+else if(temp >= 0 && temp <= 2){
+    console.log("Havo sovuq kurtka kiying");
+}
+else{
+    console.log("Havo ajoyib ko'fta kiying");
+}
+
+
+
+
+
